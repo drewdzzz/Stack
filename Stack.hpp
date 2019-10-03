65e5e30f7bd0ddf8bd2b6e0879648bfe4f90ff6a
drewdzzz <54822306+drewdzzz@users.noreply.github.com>
2019-10-03 23:18:30 +0300

    Verification checks data pointer now

--- Stack.hpp
+++ Stack.hpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <assert.h>
 
+
 #undef private:
 
 #define SET_NAME(stk)  \
@@ -14,14 +15,14 @@ stk.set_name (#stk);   \
 
 typedef int elem_t;
 #define data_dump printf("%d\n", data[i]);
-const int canary1_value    = 0xF023A4BA;
-const int canary2_value    = 0xF563F454;
-const elem_t canary3_value = ~0xAB;
-const elem_t canary4_value = ~0xC7;
-const int early_size       = 5;
-const elem_t $POISON       = ~0xAA;
-const int Stack_increase   = 10;
-const int Stack_decrease   = 13;  //decarease value must be more than increase one
+static const int canary1_value    = 0xF023A4BA;
+static const int canary2_value    = 0xF563F454;
+static const elem_t canary3_value = ~0xAB;
+static const elem_t canary4_value = ~0xC7;
+static const int early_size       = 5;
+static const elem_t $POISON       = ~0xAA;
+static const int Stack_increase   = 10;
+static const int Stack_decrease   = 13;  //decarease value must be more than increase one
 
 #ifdef TEST_MODE
 	typedef int elem_t;
@@ -30,7 +31,8 @@ const int Stack_decrease   = 13;  //decarease value must be more than increase o
 enum ERROR_CODE
 {
 	OK,
-	SUM_IS_NOT_OK,
+	DATA_SUM_IS_NOT_OK,
+	SUM_STRUCT_IS_NOT_OK,
 	STRUCT_CANARIES_FAULT,
 	DATA_CANARIES_FAULT,
 	STACK_OVERFLOW,
@@ -38,26 +40,28 @@ enum ERROR_CODE
 	ALLOCATING_ERROR
 };
 //0 - OK
-//1 - SUM IS NOT OK
+//1 - DATA SUM IS NOT OK
+//2 - SUM_STRUCT IS NOT OK
 //2 - STRUCT CANARIES ARE NOT OK
 //3 - DATA CANARIES ARE NOT OK
 //4 - STACK OVERFLOW
 //5 - STACK UNDERFLOW
 //6 - MEMORY CAN'T BE ALLOCATED
 
+
 struct Stack_t
 {
 private:
 	int canary1 = 0;         //initialized DEFEND
-	long sum1 = 0;           //initialized DEFEND
+	long data_sum = 0;       //initialized DEFEND
 	long size = 0;           //initialized
 	long max_size = 3;       //initialized
 	elem_t* data = nullptr;  //initialized
-    char* name = "";
-	long sum2 = 0;			 //initialized DEFEND
+    char* name = "YOU DIDN'T NAMED ME!!!";
+	long struct_sum = 0;	 //initialized DEFEND
 	int canary2 = 0;         //initialized DEFEND
 
-	long calculate_sum ()
+	long calc_data_sum ()
 	{
 		long sum = 0;
 		for (long i = 1; i < size; i++)
@@ -69,6 +73,19 @@ private:
 
 		}
 		sum += 13*size - max_size;
+		return sum;
+	}
+
+	long calc_struct_sum ()
+	{
+		char* adress = (char*) this;
+		long sum = 0;
+		char temp = 0;
+		for (long i = 0; i < (long)&(this->struct_sum) - (long)this; i++)
+        {
+            temp = *(adress+i);
+            sum += temp & 0x0F + temp >> 4;
+        }
 		return sum;
 	}
 
@@ -101,11 +118,17 @@ private:
 	ERROR_CODE verification ()
 	{
 	#ifdef TEST_MODE
-		long temp_sum = calculate_sum();
-		if ( temp_sum!=sum1 || temp_sum!=sum2 )
+		long temp_data_sum = calculate_sum();
+		long temp_struct_sum = calc_struct_sum();
+		if ( temp_data_sum != data_sum )
 		{
-		error = SUM_IS_NOT_OK;
-		return SUM_IS_NOT_OK;
+		error = DATA_SUM_IS_NOT_OK;
+		return DATA_SUM_IS_NOT_OK;
+		}
+		if ( temp_struct_sum != struct_sum )
+		{
+		error = SUM_STRUCT_IS_NOT_OK;
+		return SUM_STRUCT_IS_NOT_OK;
 		}
 		if ( canary1!=canary1_value || canary2!=canary2_value )
 		{
@@ -137,8 +160,10 @@ private:
 		error = OK;
 		return OK;
 	#else
-		long temp_sum = calculate_sum();
-		if ( temp_sum!=sum1 || temp_sum!=sum2 ) return SUM_IS_NOT_OK;
+		long temp_data_sum = calc_data_sum();
+		long temp_struct_sum = calc_struct_sum ();
+		if ( temp_data_sum != data_sum ) return DATA_SUM_IS_NOT_OK;
+		if ( temp_struct_sum != struct_sum ) return SUM_STRUCT_IS_NOT_OK;
 		if ( canary1!=canary1_value || canary2!=canary2_value ) return STRUCT_CANARIES_FAULT;
 		if ( data[0]!=canary3_value || data[max_size+1]!=canary4_value ) return DATA_CANARIES_FAULT;
 		if ( size > max_size + 1 ) return STACK_OVERFLOW;
@@ -157,11 +182,10 @@ private:
 		switch (error_code)
 		{
 			case (OK): printf ("OK\n"); break;
-			case (SUM_IS_NOT_OK): printf ("SUM IS NOT OK, someone has attacked you :(\n");
-			                      printf ("FIRST SUM: %ld\n", sum1);
-			                      printf ("SECOND SUM: %ld\n", sum2);
-			                      printf ("Sum must be equal: %ld\n", calculate_sum ());
-			                      break;
+			case (DATA_SUM_IS_NOT_OK): printf ("DATA SUM IS NOT OK, someone has attacked you :(\n");
+			                           printf ("DATA SUM: %ld\n", data_sum);
+			                           printf ("Data sum must be equal: %ld\n", calc_data_sum ());
+			                           break;
 			case (STRUCT_CANARIES_FAULT): printf ("STRUCT CANARIES ARE NOT OK, someone has attacked you :(\n");
 			                              printf ("Actual first canary value: %d\n", canary1);
 			                              printf ("Value that first canary must be equal: %d\n", canary1_value);
@@ -176,10 +200,14 @@ private:
 			                            break;
 			case (STACK_OVERFLOW): printf ("STACK OVERFLOWED\n"); break;
 			case (STACK_UNDERFLOW): printf ("STACKED UNDERFLOWED\n"); break;
+			case (SUM_STRUCT_IS_NOT_OK): printf ("STRUCT SUM IS NOT OK, someone has attacked you :(\n");
+			                             printf ("STRUCT SUM SUM: %ld\n", struct_sum);
+                                         printf ("Struct sum must be equal: %ld\n", calc_struct_sum ());
+			                             break;
 			case (ALLOCATING_ERROR): printf ("MEMORY FOR NEW DATA CAN'T BE ALLOCATED\n"); break;
 			default: printf ("Wrong code\n"); break;
 		}
-		if (error_code != ALLOCATING_ERROR) print_stack();
+		if (error_code != ALLOCATING_ERROR && error_code != SUM_STRUCT_IS_NOT_OK) print_stack();
 		printf ("----------------------\n");
 	}
 
@@ -191,7 +219,9 @@ public:
 
 	void print_stack()
 	{
-		printf ("STACK '%s':\n", name);
+		printf ("STACK '%s':\n"
+				"ADRESS OF STACK: %p\n", name, this);
+
 		for (long i = 1; i <= size - 1; i++)
 			if (data[i] != $POISON)
 			{
@@ -214,18 +244,20 @@ public:
 		}
 		printf ("END OF STACK\n");
 	}
+
 	void set_name (char* name)
 	{
 		this->name = name;
 	}
+
 	bool push (elem_t new_elem)
 	{
 		ERROR_CODE error_code = verification ();
 		if (error_code == OK)
 		{
             data[size++] = new_elem;
-            sum1 = calculate_sum ();
-            sum2 = calculate_sum ();
+            data_sum = calc_data_sum ();
+            struct_sum = calc_struct_sum ();
             return 1;
 		}
 		else
@@ -247,8 +279,8 @@ public:
 			size--;
 			elem_t pop_elem = data[size];
 			data[size] = $POISON;
-			sum1 = calculate_sum ();
-            sum2 = calculate_sum ();
+			data_sum = calc_data_sum ();
+            struct_sum = calc_struct_sum ();
 			return pop_elem;
 		}
 		else
@@ -266,14 +298,14 @@ public:
 	{
 		canary1 = canary1_value;
 		canary2 = canary2_value;
-		sum1 = 13 - early_size;
-		sum2 = 13 - early_size;
+		data_sum = 13 - early_size;
 		size = 1;
 		max_size = early_size;
 		data = (elem_t*) calloc (max_size+2, sizeof (elem_t));
 		for (int i = 1; i <= max_size; i++) data[i] = $POISON;
 		data[0] = canary3_value;
 		data[max_size+1] = canary4_value;
+        struct_sum = calc_struct_sum ();
 	}
 
 	~Stack_t ()
